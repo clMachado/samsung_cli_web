f8e9cf065913b3762a8bcfcd1b95c092d1c787bc
clMachado <44262034+clMachado@users.noreply.github.com>
2025-01-16 01:04:37 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true;
+  
   publicPath: process.env.NODE_ENV === 'production'
     ? '/samsung_cli_web/'
     : '/'
